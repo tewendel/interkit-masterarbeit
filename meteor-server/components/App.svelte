ff533dea3d297a7b668723fb481367ad489094d6
Holger H <holgheiss@gmail.com>
2021-02-11 13:47:50 +0100

    add carbon components to server

--- meteor-server/components/App.svelte
+++ meteor-server/components/App.svelte
@@ -1,7 +1,14 @@
 <script>
+  import "carbon-components-svelte/css/g10.css"; // all g10 g100 g90 white
 
   import Router from 'svelte-spa-router'
   import ProjectManager from './ProjectManager.svelte'
+
+  
+  import { 
+    Header,
+    Content,
+  } from "carbon-components-svelte";
   
   // see https://github.com/ItalyPaleAle/svelte-spa-router/blob/master/README.md
   const routes = {
@@ -10,4 +17,10 @@
 
 </script>
 
-<Router {routes} />
+<Header company="interkit" platformName="Rekationssystem" href="/"></Header>
+
+<Content style="background: none; padding: 1rem">
+
+    <Router {routes} />
+
+</Content>
