fec379e3ea649b92d5044ee4d5131ae9aa208fee
Holger H <holgheiss@gmail.com>
2021-03-03 17:22:49 +0100

    starter project set viewport

--- repositories/starter/App.svelte
+++ repositories/starter/App.svelte
@@ -56,6 +56,7 @@
 </ion-app>
 
 <svelte:head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <script
     type="module"
     src="https://cdn.jsdelivr.net/npm/@ionic/core/dist/ionic/ionic.esm.js">
