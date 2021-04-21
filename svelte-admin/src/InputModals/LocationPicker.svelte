7ac30884040dabb630644473b1ae6d8e3cdf3554
Sebastian Quack <Sebastian Quack>
2021-04-21 19:49:20 +0200

    improve location picker

--- svelte-admin/src/InputModals/LocationPicker.svelte
+++ svelte-admin/src/InputModals/LocationPicker.svelte
@@ -13,7 +13,30 @@ import {
   import 'leaflet/dist/leaflet.css';
 
   export let open = false;
-  export let value = {lat: 51.505, lng: -0.09};
+  export let value;
+  
+  
+  let center;
+  if(value) {
+    center = value // normally center map on marker
+  } else { 
+    try {
+      center = JSON.parse(localStorage.getItem("locationPickerMapPosition"))
+    } catch(e) {
+      console.log(e)
+      center = null;
+    }
+    console.log(center)
+  }
+  if(!center) {
+    center = {lat: 51.505, lng: -0.09} // default map center
+  }
+
+  let zoom;
+  zoom = localStorage.getItem("locationPickerMapZoom")
+  if(!zoom) {
+    zoom = 13;
+  }
 
   export let submit;
   export let close;
@@ -21,22 +44,49 @@ import {
   let map;
   let marker;
 
+  const setupMarker = (latLng) => {
+    if(marker)
+      map.removeLayer(marker);
+    marker = L.marker([latLng.lat, latLng.lng], {draggable: true}).addTo(map)
+    value = latLng;
+    marker.on('dragend', function(event) {
+      value = event.target.getLatLng();
+      console.log(value.lat, value.lng)
+    });
+  }
+
   onMount(()=>{
     L.Icon.Default.imagePath = 'leaflet/'
 
-    map = L.map('mapid').setView([value.lat, value.lng], 13);  
+    map = L.map('mapid').setView([center.lat, center.lng], zoom);  
 
     L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
       maxZoom: 20,
       attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    marker = L.marker([51.5, -0.09], {draggable: true}).addTo(map)
-
-    marker.on('dragend', function(event) {
-      value = event.target.getLatLng();
-      console.log(value.lat, value.lng)
+    map.on('dragend',function(e){
+      console.log(map.getCenter())
+      localStorage.setItem("locationPickerMapPosition", JSON.stringify(map.getCenter()))
     });
+
+    map.on('zoomend', function() {
+      localStorage.setItem("locationPickerMapZoom", map.getZoom())
+    });
+
+    map.on('click', function(e) {        
+        let clickLocation = e.latlng;
+        if(!marker) {
+          setupMarker(clickLocation);
+        } else {
+          value = clickLocation;
+          marker.setLatLng(clickLocation); 
+        }
+    });
+
+    if(value) {
+      setupMarker(value);
+    }
   
   })
 
@@ -46,6 +96,29 @@ import {
       map.setView(value)
     }
   }
+
+  let geocodingQuery = null;
+  const doGeocoding = async () => {
+    if(geocodingQuery?.length) {
+      console.log(geocodingQuery)
+      let response = await fetch(`https://nominatim.openstreetmap.org/search?q=${geocodingQuery}&format=json`);
+      let responseJSON = await response.json()
+      console.log(responseJSON);
+      if(responseJSON?.[0])
+        map.setView({lat: responseJSON[0].lat, lng: responseJSON[0].lon})
+    }
+  }
+
+  const onKeyPress = e => {
+    if (e.charCode === 13) doGeocoding();
+  }
+
+  const moveToMarker = () => {
+    if(value) {
+      map.setView(value);
+    }
+    
+  }
   
 </script>
 
@@ -54,10 +127,14 @@ import {
   on:submit={()=>{open = false; submit()}}
   on:close={close}
   >
-  <ModalHeader title="Drag the marker to choose a location" />
+  <ModalHeader title="Drag the marker or click on the map to choose a location" />
   <ModalBody>
 
      <div id="mapid"></div>
+
+     <input type="text" bind:value={geocodingQuery} on:keypress={onKeyPress}/>
+     <button on:click={doGeocoding}>move map</button>
+     <button on:click={moveToMarker}>back to marker</button>
     
   </ModalBody>
   <ModalFooter primaryButtonText="Save"/>
@@ -65,5 +142,5 @@ import {
 
 
 <style>
-  #mapid { height: 180px; }
+  #mapid { height: 180px; margin-bottom: 20px;}
 </style>
