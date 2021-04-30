0b37cd324b7de5000792e53af25f11636657bc20
Sebastian Quack <Sebastian Quack>
2021-04-30 22:54:51 +0200

    disable manual cors headers

--- meteor-server/imports/importServer.js
+++ meteor-server/imports/importServer.js
@@ -9,10 +9,10 @@ import { importProjectData, replaceProjectData } from './projectUtils'
 
 const importData = async (req, res) => {
 
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  /*res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");*/
 
 
   //if (!request.query.uploadToken || !uploadTokens.has(request.query.uploadToken)) {
--- meteor-server/imports/mediaServer.js
+++ meteor-server/imports/mediaServer.js
@@ -108,11 +108,11 @@ export const setupMediaServer = (app) => {
 
   app.post('/mediaUpload', upload.single('mediafile'), async (req, res) => {
 
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    /*res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");*/
+    
     if (req.file !== undefined /*&& req.file.mimetype.substr(0, 6) == 'image/'*/) {
     
         _fs.stat(req.file.path, function (_statError, _statData) { 
