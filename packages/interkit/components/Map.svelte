1f695cc84a3a9ef66e23cd6d38f652fde47d9cc7
Sebastian Quack <Sebastian Quack>
2021-05-20 15:04:32 +0200

    bugifx hide labels

--- packages/interkit/components/Map.svelte
+++ packages/interkit/components/Map.svelte
@@ -216,14 +216,14 @@
       //attribution: cartodbAttr
     }).addTo(map)*/
 
-    const Stamen_TonerLabels = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-labels/{z}/{x}/{y}{r}.{ext}', {
+    labels_layer = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-labels/{z}/{x}/{y}{r}.{ext}', {
       /*attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',*/
       subdomains: 'abcd',
       minZoom: 0,
       maxZoom: 20,
       ext: 'png'
     });
-    Stamen_TonerLabels.addTo(map)
+    labels_layer.addTo(map)
 
     // load the marker icon
     if(markerIconAsset) {
