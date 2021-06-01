d0f0706fdc7f614ccb9940e5997f08a2e83ae2a7
Sebastian Quack <Sebastian Quack>
2021-06-01 18:53:31 +0200

    key for audioplayer expanded title2

--- packages/interkit/components/AudioPlayer.svelte
+++ packages/interkit/components/AudioPlayer.svelte
@@ -217,9 +217,12 @@
         </figure>
       {/if}
 
+      {#key title}
       <h3 class="AudioPlayer__Expanded__Title">
         {title}
       </h3>
+      {/key}
+      
       <h4 class="AudioPlayer__Expanded__SubTitle">
         <span>
           {util.rowValString(categoryRow, categoryColumns[categoryIndex].titleColumn)}
