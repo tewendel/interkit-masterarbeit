d95d2a43b6babc5dddbc1588e9540dccd1575b86
Sebastian Quack <Sebastian Quack>
2021-10-01 13:17:21 +0200

    qr scanner tips

--- packages/interkit/components/QRScanner.svelte
+++ packages/interkit/components/QRScanner.svelte
@@ -17,7 +17,15 @@ let loading = true;
 let showTips = false;
 
 export let elementKeyColumn; // the column on a sheet to select an element (optional)
-export let targetKey; // a key that we use as target element (optional)
+
+// columns for tips sheet
+export let tipQrKeyColumn;
+export let tipImageColumn;
+export let tipTextColumn;
+export let tipOrderColumn;
+
+const QRElementStore = InterkitClient.getGlobalStore("QRElement") // this is a store
+const targetElement = QRElementStore ? $QRElementStore : undefined
 
 let dataRows; 
 
@@ -37,7 +45,7 @@ const onScan = (code) => {
   let payload = {
     code,
     elementRow,
-    targetFound: elementRow ? (util.rowVal(elementRow, elementKeyColumn) == targetKey) : false
+    targetFound: elementRow ? (elementRow.key == targetElement?.key) : false
   }
 
   executeTrigger("QRCodeScanned", payload);
@@ -133,6 +141,10 @@ onDestroy(()=>{
 
 });
 
+const close = () => {
+  showTips = false;
+}
+
 </script>
 
 <div id="scanner-container">
@@ -140,9 +152,11 @@ onDestroy(()=>{
   {#if loading}
     <div class="loadingMessage" hidden="">⌛ Warte auf Kamera...</div>
   {:else}
-    <div class="tip-button-container">
-      <Button text="Hinweis" onClick={()=>showTips = true}/>
-    </div>
+    {#if targetElement}
+      <div class="tip-button-container">
+        <Button text="Hinweise zeigen" onClick={()=>showTips = true}/>
+      </div>
+    {/if}
   {/if}
 </div>
 
@@ -152,10 +166,18 @@ onDestroy(()=>{
       headline="QR-Code Scannen"
     >
       <svelte:fragment slot="left">
-        <Button text="<" onClick={()=>showTips = false}/>
+        <Button text="<" onClick={close}/>
       </svelte:fragment>
       <svelte:fragment slot="content">
-        <QRTips/>
+        <QRTips 
+          {targetElement}
+          {elementKeyColumn}
+          {tipQrKeyColumn}
+          {tipImageColumn}
+          {tipTextColumn}
+          {tipOrderColumn}
+          onClose={close}
+        />
       </svelte:fragment>
     </TopNavBarCustom>
   </Overlay>
