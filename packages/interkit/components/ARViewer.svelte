34b35c01ccc3bcb21dee324f45501f3710df8b33
Holger H <holgheiss@gmail.com>
2021-10-01 15:56:50 +0200

    interkit: ARViewer

--- packages/interkit/components/ARViewer.svelte
+++ packages/interkit/components/ARViewer.svelte
@@ -6,12 +6,15 @@
   import MediaFileResolver from './MediaFileResolver.svelte'
   import MarkdownContent from './MarkdownContent.svelte'
   import Button from './Button.svelte'
+  import { executeTrigger } from '../actions'
 
   export let titleColumn; // title column
   export let glbColumn; // glb (android) column
   export let usdzColumn; // usdz (ios) column
-  export let descriptionColumn; //  description column
-  export let ARmode = "AR preferred" // "AR preferred" | "AR only"
+  export let imageColumn; // image column
+  export let videoColumn; // video column
+  export let closeTrigger; // triggered at close
+  export let ARmode = "only" // "preferred" | "only"
 
   const ARElementStore = InterkitClient.getGlobalStore("ARElement")
   let element
@@ -20,7 +23,7 @@
   let androidLinkRef
 
   const androidFallbackUrl = "https://developers.google.com/ar"
-  const androidARmode = ARmode === "AR only" ? "ar_only" : "ar_preferred"
+  const androidARmode = ARmode === "only" ? "ar_only" : "ar_preferred"
 
   const generateAndroidHref = url => 
     `intent://` + 
@@ -30,15 +33,38 @@
     `#Intent;scheme=https;package=com.google.android.googlequicksearchbox;action=android.intent.action.VIEW;` + 
     `S.browser_fallback_url=${androidFallbackUrl};end;`
 
+  let mode // video | ios | android
 
   onMount(()=> {
     const a = document.createElement("a");
     if (a.relList.supports("ar")) {
       // iOS quickloor AR is available.
-      iosLinkRef.click()
+      mode = "ios"
     } else {
+      const userAgent = window.navigator.userAgent
+      if (/Android/i.test(userAgent)) {
+        // android 
+        mode = "android"
+      } else {
+        mode = "video"
+      }
+    } 
 
-    }
+     // start automatically
+    setTimeout(() => {
+      if (mode === "android") androidLinkRef.click()
+      if (mode === "ios") iosLinkRef.click()
+      }, 1000
+    )
+
+    // close automatically
+    setTimeout(() => {
+      if (mode !== "video") {
+          executeTrigger(closeTrigger)
+        }
+      }, 2000
+    )
+
   })
 
   $: {
@@ -46,61 +72,69 @@
       title: util.rowVal($ARElementStore, titleColumn),
       glbFileRef: util.rowVal($ARElementStore, glbColumn),
       usdzFileRef: util.rowVal($ARElementStore, usdzColumn),
+      imageFileRef: util.rowVal($ARElementStore, imageColumn),
+      videoFileRef: util.rowVal($ARElementStore, videoColumn),
     }
+    console.log(element)
   }
 
 </script>
 
 <div class="ARViewer container">
   {#if element}
-    <ul>
-      <li>
-        AR VIEW
-      </li>
-      <li>
-        <MediaFileResolver let:url mediafileRef={element.glbFileRef} >
-          <a 
-            bind:this={androidLinkRef} 
-            rel="external" 
-            title={element.title} 
-            href={generateAndroidHref(url)}
-          >
-            <Button inverse>
-              start android
-            </Button>
-          </a>
+    {#if mode == "video"}
+      <!-- svelte-ignore a11y-media-has-caption -->
+      <video autoplay muted loop>
+        <MediaFileResolver let:url mediafileRef={element.videoFileRef} >
+          <source src={url} >
         </MediaFileResolver>
-      </li>
-      <li>
-        <MediaFileResolver let:url mediafileRef={element.usdzFileRef} >
-          <a bind:this={iosLinkRef} rel="ar external" title={element.title} href={url} >
-            <Button inverse>
-              start iOS
-            </Button>
-          </a>
-        </MediaFileResolver>
-      </li>
-    </ul>
+      </video>
+    {:else}
+      <ul>
+        <li>
+          AR VIEW
+          mode={mode}
+        </li>
+        <li>
+          <MediaFileResolver let:url mediafileRef={element.glbFileRef} >
+            <a 
+              bind:this={androidLinkRef} 
+              rel="external" 
+              title={element.title} 
+              href={generateAndroidHref(url)}
+            >
+              <Button inverse>
+                start android
+              </Button>
+            </a>
+          </MediaFileResolver>
+        </li>
+        <li>
+          <MediaFileResolver let:url mediafileRef={element.usdzFileRef} >
+            <a bind:this={iosLinkRef} rel="ar" title={element.title} href={url} >
+              <MediaFileResolver let:url={imgUrl} mediafileRef={element.imageFileRef} >
+                <img style="display:inline" src={imgUrl} alt={element.title}/>
+              </MediaFileResolver>
+            </a>
+          </MediaFileResolver>
+        </li>
+      </ul>
+    {/if}
   {/if}
 </div>
 
 <style>
   .container {
-    font-size: 14px;
-    line-height: 20px;
-    padding: 16px;
     width: 100%;
     height: 100%;
     background-color: rgba(255,255,255,0.8);
   }
 
-  ul {
-    display: flex;
+  video {
     height: 100%;
-    flex-direction: column;
-    justify-content: space-evenly;
-    text-align: center;
-    font-size: 12vw;
+    width: 100%;
+    object-fit: cover;
   }
+
 
 </style>
