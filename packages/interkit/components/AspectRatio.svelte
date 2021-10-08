dc007ef53dc829b9a207c4261ccfe98496b2248f
Holger H <holgheiss@gmail.com>
2021-10-08 14:25:16 +0200

    interkit: imporove AspectRatio

--- packages/interkit/components/AspectRatio.svelte
+++ packages/interkit/components/AspectRatio.svelte
@@ -5,6 +5,8 @@
   export let aspectRatio = null // manual setting
   export let standalone = false // add border
 
+  if (typeof standalone == "string") standalone = standalone === "TRUE" // blockly conversion
+
   const aspectRatioTypes = {
     small_overlay: .46,
     element: .55,
@@ -18,30 +20,33 @@
 
 </script>
 
-<div class:standalone style={`--aspect-ratio: ${ aspectRatio * 100 }%`}>
-
+<div class:standalone class="AspectRatio container" style={`--aspect-ratio: ${ aspectRatio * 100 }%`}>
+  <div class="inner-container">
+    <slot />
+  </div>
 </div>
 
 <style>
-  .aspect-ratio-box {
-    background: white;
-  }
-  .aspect-ratio-box::before {
-    content: "";
-    width: 1px;
-    margin-left: -1px;
-    float: left;
+  .container {
     height: 0;
+    overflow: hidden;
     padding-top: var(--aspect-ratio);
-  }
-  .aspect-ratio-box::after { /* to clear float */
-    content: "";
-    display: table;
-    clear: both;
+    background: white;
+    position: relative;
+    width: 100%;
+    box-sizing: border-box;
   }
 
+  .inner-container {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
   .standalone {
     border: 1px solid black;
     border-radius: 20px;
+    overflow: hidden;
   }
 </style>
--- packages/interkit/components/MapSimple.svelte
+++ packages/interkit/components/MapSimple.svelte
@@ -381,6 +381,8 @@
 
   .container {
     width: 100%;
+    height: 100%;
+    flex:1;
   }
 
   .map { 
