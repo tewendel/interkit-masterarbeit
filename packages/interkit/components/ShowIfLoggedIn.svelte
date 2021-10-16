9567e7be77b68464fd0504ac7dee04ee4a2be354
Holger H <holgheiss@gmail.com>
2021-10-16 13:25:17 +0200

    interkit: add ShowIfLoggedIn component

---
+++ packages/interkit/components/ShowIfLoggedIn.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { InterkitClient } from '../'
+
+  let userId = InterkitClient.userId;
+
+  $:  console.log($userId)
+
+</script>
+
+{#if $userId}
+  <slot name="iftrue">
+  </slot>
+{:else}
+  <slot name="else">
+  </slot>
+{/if}
