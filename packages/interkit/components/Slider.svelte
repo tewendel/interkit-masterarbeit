24a84ab1e1a18c2c94df6c2ba4c8f6db3d4892f7
Jakob Wierzba <interkitgitlab@jakobwierzba.de>
2021-10-26 15:13:57 +0200

    fix social slider tiny white bottom border

--- packages/interkit/components/Slider.svelte
+++ packages/interkit/components/Slider.svelte
@@ -41,7 +41,8 @@
     width: 85%;
     flex: none;
     scroll-snap-align: center;
-    box-sizing: border-box;
+    /* important that they all are exactly the same width (only the first one has a border-left), otherwise it confuses AspectRatio */
+    box-sizing: content-box;
     overflow: hidden; /* so border-radius works, TODO check iOS */
     border: var(--border-width) solid var(--border-color);
   }
