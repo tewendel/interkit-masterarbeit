62e5ba113bffe6ec3ff5b14839e8dfef49431682
Holger H <holgheiss@gmail.com>
2021-12-29 16:29:31 +0100

    improve login form

--- svelte-admin/src/Login.svelte
+++ svelte-admin/src/Login.svelte
@@ -4,17 +4,18 @@
   let username = "admin"
   let password = "password";
 
-  const submit = async () => {
+  const submit = async e => {
+    e.preventDefault();
     await InterkitClient.login({username, password})
   }
 
 </script>
 
-<div>  
+<form on:submit={submit}>  
   <input bind:value={username} type="text"/><br/>
   <input bind:value={password} type="password"/><br/>
-  <button on:click={submit}>login</button>
-</div>
+  <input type="submit" value="login">
+</form>
 
 <style>
   div {
