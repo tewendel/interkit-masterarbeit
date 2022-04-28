a93f2848d93f2ae7e310556621a207425bd997f0
Holger H <holgheiss@gmail.com>
2022-04-28 12:45:06 +0200

    [interkit] update icon component with new icons #191

--- packages/interkit/components/Icon.svelte
+++ packages/interkit/components/Icon.svelte
@@ -157,4 +157,78 @@
     background-image: url("../icons/Full-Screen-Full.svg");
   }
 
+  /* figma import Full/ */
+
+  .icon-Full-AR           { background-image: url("../icons/Full/AR.svg"); }
+  .icon-Full-Archiv       { background-image: url("../icons/Full/Archiv.svg"); }
+  .icon-Full-Bookmark     { background-image: url("../icons/Full/Bookmark.svg"); }
+  .icon-Full-Camera       { background-image: url("../icons/Full/Camera.svg"); }
+  .icon-Full-Chat         { background-image: url("../icons/Full/Chat.svg"); }
+  .icon-Full-Check        { background-image: url("../icons/Full/Check.svg"); }
+  .icon-Full-Close-Circle { background-image: url("../icons/Full/Close-Circle.svg"); }
+  .icon-Full-Copy         { background-image: url("../icons/Full/Copy.svg"); }
+  .icon-Full-Dashboard    { background-image: url("../icons/Full/Dashboard.svg"); }
+  .icon-Full-Date         { background-image: url("../icons/Full/Date.svg"); }
+  .icon-Full-Drag         { background-image: url("../icons/Full/Drag.svg"); }
+  .icon-Full-FullScreen   { background-image: url("../icons/Full/FullScreen.svg"); }
+  .icon-Full-GroupChat    { background-image: url("../icons/Full/GroupChat.svg"); }
+  .icon-Full-Help         { background-image: url("../icons/Full/Help.svg"); }
+  .icon-Full-Layer        { background-image: url("../icons/Full/Layer.svg"); }
+  .icon-Full-List         { background-image: url("../icons/Full/List.svg"); }
+  .icon-Full-Location     { background-image: url("../icons/Full/Location.svg"); }
+  .icon-Full-Map          { background-image: url("../icons/Full/Map.svg"); }
+  .icon-Full-Menu         { background-image: url("../icons/Full/Menu.svg"); }
+  .icon-Full-Microphone   { background-image: url("../icons/Full/Microphone.svg"); }
+  .icon-Full-More-1       { background-image: url("../icons/Full/More-1.svg"); }
+  .icon-Full-More         { background-image: url("../icons/Full/More.svg"); }
+  .icon-Full-Pause        { background-image: url("../icons/Full/Pause.svg"); }
+  .icon-Full-Phaenomen    { background-image: url("../icons/Full/Phaenomen.svg"); }
+  .icon-Full-Play         { background-image: url("../icons/Full/Play.svg"); }
+  .icon-Full-QR-Scan      { background-image: url("../icons/Full/QR-Scan.svg"); }
+  .icon-Full-Send         { background-image: url("../icons/Full/Send.svg"); }
+  .icon-Full-Settings     { background-image: url("../icons/Full/Settings.svg"); }
+  .icon-Full-SingleChat   { background-image: url("../icons/Full/SingleChat.svg"); }
+  .icon-Full-Social       { background-image: url("../icons/Full/Social.svg"); }
+  .icon-Full-Splitscreen  { background-image: url("../icons/Full/Splitscreen.svg"); }
+  .icon-Full-Warning      { background-image: url("../icons/Full/Warning.svg"); }
+  .icon-Full-to_Gallery   { background-image: url("../icons/Full/to_Gallery.svg"); }
+
+  /* figma import Thin/ */
+
+  .icon-Thin-AR           { background-image: url("../icons/Thin/AR.svg"); }
+  .icon-Thin-Archiv       { background-image: url("../icons/Thin/Archiv.svg"); }
+  .icon-Thin-Arrow-Left   { background-image: url("../icons/Thin/Arrow-Left.svg"); }
+  .icon-Thin-Arrow-Right  { background-image: url("../icons/Thin/Arrow-Right.svg"); }
+  .icon-Thin-Bookmark     { background-image: url("../icons/Thin/Bookmark.svg"); }
+  .icon-Thin-Camera       { background-image: url("../icons/Thin/Camera.svg"); }
+  .icon-Thin-Chat         { background-image: url("../icons/Thin/Chat.svg"); }
+  .icon-Thin-Check        { background-image: url("../icons/Thin/Check.svg"); }
+  .icon-Thin-Close-Circle { background-image: url("../icons/Thin/Close-Circle.svg"); }
+  .icon-Thin-Close        { background-image: url("../icons/Thin/Close.svg"); }
+  .icon-Thin-Copy         { background-image: url("../icons/Thin/Copy.svg"); }
+  .icon-Thin-Dashboard    { background-image: url("../icons/Thin/Dashboard.svg"); }
+  .icon-Thin-Dropdown-Up  { background-image: url("../icons/Thin/Dropdown-Up.svg"); }
+  .icon-Thin-Dropdown     { background-image: url("../icons/Thin/Dropdown.svg"); }
+  .icon-Thin-Filter       { background-image: url("../icons/Thin/Filter.svg"); }
+  .icon-Thin-Forward-15   { background-image: url("../icons/Thin/Forward-15.svg"); }
+  .icon-Thin-FullScreen   { background-image: url("../icons/Thin/FullScreen.svg"); }
+  .icon-Thin-Hint         { background-image: url("../icons/Thin/Hint.svg"); }
+  .icon-Thin-Layer        { background-image: url("../icons/Thin/Layer.svg"); }
+  .icon-Thin-Location     { background-image: url("../icons/Thin/Location.svg"); }
+  .icon-Thin-Map          { background-image: url("../icons/Thin/Map.svg"); }
+  .icon-Thin-Menu         { background-image: url("../icons/Thin/Menu.svg"); }
+  .icon-Thin-Microphone   { background-image: url("../icons/Thin/Microphone.svg"); }
+  .icon-Thin-Minus        { background-image: url("../icons/Thin/Minus.svg"); }
+  .icon-Thin-Pause        { background-image: url("../icons/Thin/Pause.svg"); }
+  .icon-Thin-Phaenomen    { background-image: url("../icons/Thin/Phaenomen.svg"); }
+  .icon-Thin-Play         { background-image: url("../icons/Thin/Play.svg"); }
+  .icon-Thin-Plus         { background-image: url("../icons/Thin/Plus.svg"); }
+  .icon-Thin-Position     { background-image: url("../icons/Thin/Position.svg"); }
+  .icon-Thin-QR-Scan      { background-image: url("../icons/Thin/QR-Scan.svg"); }
+  .icon-Thin-Replay-30    { background-image: url("../icons/Thin/Replay-30.svg"); }
+  .icon-Thin-Settings     { background-image: url("../icons/Thin/Settings.svg"); }
+  .icon-Thin-Social       { background-image: url("../icons/Thin/Social.svg"); }
+  .icon-Thin-to_Gallery   { background-image: url("../icons/Thin/to_Gallery.svg"); }
+  .icon-Thin-zoom-In      { background-image: url("../icons/Thin/zoom-In.svg"); }
+
 </style>
