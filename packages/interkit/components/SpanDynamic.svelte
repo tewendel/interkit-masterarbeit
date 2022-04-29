7eb5ecaa8132ae694d2517f0f6906b34939d1d42
Sebastian Quack <Sebastian Quack>
2022-04-29 10:55:22 +0200

    add span dynamic component

---
+++ packages/interkit/components/SpanDynamic.svelte
@@ -0,0 +1,26 @@
+<script>
+
+    import { InterkitClient } from '../'
+    import util from '../util.js'
+
+    export let globalStore
+    export let sheetColumn
+
+    console.log("property", sheetColumn)
+    let element = globalStore ? InterkitClient.getGlobalStore(globalStore) : null;
+    let spanValue;
+
+    $: {
+        if($element?.values) {
+            spanValue = $element?.values[util.colKey(sheetColumn)]
+        }
+    }
+
+
+</script>
+
+{#if spanValue}
+    <span>
+        {spanValue}
+    </span>
+{/if}
