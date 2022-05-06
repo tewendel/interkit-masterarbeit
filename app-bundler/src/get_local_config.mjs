3eb87d78772240bf96b8b5df1ef265fa11dbd0f9
Holger H <holgheiss@gmail.com>
2022-05-06 10:29:13 +0200

    indicate "local" version

--- app-bundler/src/get_local_config.mjs
+++ app-bundler/src/get_local_config.mjs
@@ -3,7 +3,7 @@ import { generateInterkitConfig } from './filesystem.mjs'
 const get_local_config =  async (req, res) => {
   const projectSlug = req.params.projectSlug;
   let config = generateInterkitConfig({slug: projectSlug})
-  res.send(config)
+  res.send({...config, bundle_version: "local"})
 }
 
 export { get_local_config }
