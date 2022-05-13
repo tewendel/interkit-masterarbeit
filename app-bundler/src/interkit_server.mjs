95e45a159c0963c24e6418181834e386e3ab5f01
Holger H <holgheiss@gmail.com>
2022-05-13 17:33:47 +0200

    use interkit/interkit-connect.js in bundler

--- app-bundler/src/interkit_server.mjs
+++ app-bundler/src/interkit_server.mjs
@@ -1,39 +1,15 @@
-import simpleDDP from 'simpleddp'; // ES6
-import ws from 'isomorphic-ws';
+import { setup as serverSetup } from 'interkit/interkit-connect.js';
 import { ensureRepositories } from './filesystem.mjs'
 import { updateProjectServers } from './project_server.mjs'
 import { runUpdaters } from './updater.mjs'
-
-const opts = {
-  endpoint: process.env.INTERKIT_SERVER_SERVERSIDE_WEBSOCKETS_URL || process.env.INTERKIT_SERVER_WEBSOCKETS_URL,
-  SocketConstructor: ws,
-  reconnectInterval: 5000
-};
 
 let projects = []
 let server = null
 
 const setup = async () => {
 
-  console.log("connecting to " + opts.endpoint + "...")
-
-  server = new simpleDDP(opts);
-
-  server.on('connected', () => {
-    // do something
-    console.log("connected to interkit server")
-  });
-
-  server.on('disconnected', () => {
-    // for example show alert to user
-    console.log("disconnected from interkit server")
-  });
-
-  server.on('error', (e) => {
-    // global errors from server
-    console.log("interkit server error", e)
-  });
-
+  server = await serverSetup()
+  
   let projectsSub = server.subscribe("projects");
 
   await projectsSub.ready();
