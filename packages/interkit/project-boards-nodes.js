e01fd0d6ea1eb480f10802097b7ad4c27a49ba5c
Jakob Wierzba <interkitgitlab@jakobwierzba.de>
2022-06-24 14:33:31 +0200

    extends /boards (list) api
    query string ?nodes returns verbosely

--- packages/interkit/project-boards-nodes.js
+++ packages/interkit/project-boards-nodes.js
@@ -120,11 +120,29 @@ const api = {
   - handle: path to handlers directory
   returns list of boards
 */
-lib.boards.list = handle => fs.readdir(handle)
-  .then(allFiles => allFiles
-    .map(_ => _.match(boardFileNameRE)?.[1])
-    .filter(_ => _ !== undefined)
-  )
+lib.boards.list = (handle, params, req) => {
+  if (req?.query?.nodes) {
+    return fs.readdir(handle)
+      .then(allFiles => ({
+        boards: allFiles
+          .map(_ => _.match(boardFileNameRE)?.[1])
+          .filter(_ => _ !== undefined),
+        nodes: allFiles
+          .map(_ => {
+            const m = _.match(nodeFileNameRE)
+            if (!m) return
+            return { boardId: m[1], nodeId: m[2] }
+          })
+          .filter(_ => _ !== undefined)
+      }))
+  } else {
+    return fs.readdir(handle)
+      .then(allFiles => allFiles
+        .map(_ => _.match(boardFileNameRE)?.[1])
+        .filter(_ => _ !== undefined)
+      )
+  }
+}
 
 api.boards.list = expressify(lib.boards.list)
 
