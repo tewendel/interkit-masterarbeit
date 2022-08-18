7e355fb3dd137d7a656e714ff5007ef8ac85df26
Sebastian Quack <Sebastian Quack>
2022-08-18 16:45:39 +0200

    prototype of media mosaic

---
+++ packages/interkit/components/MediaMosaic.svelte
@@ -0,0 +1,82 @@
+<script>
+  import { InterkitClient, util } from '../'
+  import { onMount, getContext, onDestroy } from 'svelte'
+  import { executeTrigger } from '../actions'
+  import Button from './Button.svelte'
+  import Icon from './Icon.svelte'
+
+  import MediaFileImage from './MediaFileImage.svelte';
+  import AspectRatio from './AspectRatio.svelte';
+
+  // name of the trigger to activate on select
+  export let selectTrigger
+
+  export let imageColumn;
+  export let audioColumn;
+
+  export let slider = false // add border
+  if (typeof slider == "string") slider = slider === "TRUE" // blockly conversion
+
+  /*
+    @example
+    <Button>
+      Text
+    </Button>
+  */
+
+  let elementsContext = getContext("elementsProvider");
+  if(!elementsContext) alert("MediaMosaic needs elementsContextProvider as parent");
+  let elements = elementsContext?.elements;
+
+  const onClick = (element) => {
+
+    console.log("onClick", element)
+    
+    // also trigger the action, if set
+    if(selectTrigger)
+      executeTrigger(selectTrigger, element.row)
+  }
+
+</script>
+
+{#if $elements}
+  {#if $elements.length == 0}
+    <slot name="emptyElement"></slot>
+  {:else}
+    <div class="grid-wrapper" class:slider>
+      {#each $elements as element}
+        <div class="grid-item" on:click={()=>{onClick(element)}}>
+          <AspectRatio aspectRatio={0.74}>
+            <MediaFileImage fitDimension="both" objectFit="cover" mediafileRef={ util.rowVal(element.row, imageColumn) } />  
+          </AspectRatio>
+        </div>
+      {/each}
+      </div>
+  {/if}
+
+{/if}
+
+
+<style>
+
+  .grid-wrapper {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+
+  .grid-wrapper.slider {
+    display: flex;
+    flex-flow: row nowrap;
+    overflow: auto;
+    scrollbar-width: none;
+    width: 100%;
+    border-radius: var(--border-radius);
+    border: var(--border-width) solid var(--border-color);
+  }
+
+  .grid-wrapper.slider .grid-item {
+    width: 33%;
+    flex-shrink: 0;
+  }
+  
+</style>
