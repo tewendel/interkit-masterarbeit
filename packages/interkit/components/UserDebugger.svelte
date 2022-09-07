9ba225a93e42dc3bfbe1ba94fba2d9aeb6032d9a
Holger H <holgheiss@gmail.com>
2022-09-07 07:36:12 +0200

    add UserDebugger component

---
+++ packages/interkit/components/UserDebugger.svelte
@@ -0,0 +1,27 @@
+<script>
+
+  import { InterkitClient } from '../'
+  import { get } from "svelte/store"
+
+  const projectDataStore = InterkitClient.userProjectDataStore
+
+</script>
+
+<div class="UserDebugger frame">
+  <pre>
+    {JSON.stringify($projectDataStore, null, 2)}
+  </pre>
+</div>
+
+
+<style>
+  .frame {
+  }
+
+  pre {
+    white-space: pre;
+    overflow-x: auto;
+    overflow-y: visible;
+    width: 100%;
+  }
+</style>
