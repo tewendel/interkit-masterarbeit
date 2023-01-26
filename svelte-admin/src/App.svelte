bf6bad30fcf2fc7af649a8a99ae070e7492585f1
Holger H <holgheiss@gmail.com>
2023-01-26 15:05:38 +0100

    admin improve login screen

--- svelte-admin/src/App.svelte
+++ svelte-admin/src/App.svelte
@@ -14,7 +14,8 @@
     Content,
     HeaderNav,
     HeaderNavMenu,
-    HeaderNavItem
+    HeaderNavItem,
+    InlineLoading,
   } from "carbon-components-svelte";
   
   // see https://github.com/ItalyPaleAle/svelte-spa-router/blob/master/README.md
@@ -23,52 +24,71 @@
   }
 
   import { InterkitClient } from 'interkit'
+
   onMount(async ()=>{
     await InterkitClient.connect(INTERKIT_SERVER_WEBSOCKETS_URL);
     await BundleServer.connect(await InterkitClient.call("bundler.getUrl"))
   })
 
+  $: connected = InterkitClient.connected;
+
   let userId = InterkitClient.userId;
 
 </script>
 
-<Header 
-  company="interkit" 
-  platformName={$currentProjectName || "Redaktionssystem"} 
-  href="/"
-  >
-  <!--HeaderNav>
-    <HeaderNavItem text="Projekt" />
-    <HeaderNavItem href="/" text="Link 2" />
-    <HeaderNavItem href="/" text="Link 3" />
-    <HeaderNavMenu text="Menu">
-      <HeaderNavItem href="/" text="Link 1" />
+{#if $userId}
+  <Header 
+    company="interkit" 
+    platformName={$currentProjectName || "Redaktionssystem"} 
+    href="/"
+    >
+    <!--HeaderNav>
+      <HeaderNavItem text="Projekt" />
       <HeaderNavItem href="/" text="Link 2" />
       <HeaderNavItem href="/" text="Link 3" />
-    </HeaderNavMenu>
-  </HeaderNav-->
-  <div class="status">
-    {#if $userId}
-      <SystemStatusBar currentProjectServerStatus={$currentProjectServerStatus} />
+      <HeaderNavMenu text="Menu">
+        <HeaderNavItem href="/" text="Link 1" />
+        <HeaderNavItem href="/" text="Link 2" />
+        <HeaderNavItem href="/" text="Link 3" />
+      </HeaderNavMenu>
+    </HeaderNav-->
+    <div class="status">
+      {#if $userId}
+        <SystemStatusBar currentProjectServerStatus={$currentProjectServerStatus} />
+      {/if}
+    </div>
+  </Header>
+
+  <!-- set transform: none; to allow modal to be position fixed -->
+  <Content style="padding:0;">  
+
+    <Router {routes} />
+
+  </Content>
+{:else}
+  <div class="centered">
+    <h1 style="padding-bottom: 1em; text-align: center;">
+      Welcome to Interkit
+    </h1>
+    {#if $connected}
+      <Login/>
+    {:else}
+      <InlineLoading style="width: auto" description="Connecting to {INTERKIT_SERVER_WEBSOCKETS_URL}" />
     {/if}
   </div>
-</Header>
-
-<!-- set transform: none; to allow modal to be position fixed -->
-<Content style="background: none; transform: none; padding: 0;">  
-
-  {#if $userId}
-    <Router {routes} />
-  {:else}
-    <Login/>
-  {/if}
-
-</Content>
+{/if}
 
 <style>
   .status {
     flex: 1;
     padding: 1em;
     text-align: right;
+  }
+  .centered {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 80vh;
   }
 </style>
