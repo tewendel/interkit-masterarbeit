b9c18c7d4d2f7e3b6d67f0cbacc66e5ee074711d
Sebastian Quack <Sebastian Quack>
2023-01-26 20:17:09 +0100

    improve extraProps example

--- packages/interkit/components/HeadlinePage.svelte
+++ packages/interkit/components/HeadlinePage.svelte
@@ -1,10 +1,15 @@
 <script>
 
   export let headline = "test headline"
+  export let subtitle
 
 </script>
 
 <h1 class="HeadlinePage">{headline}</h1>
+
+{#if subtitle}
+<h2>{subtitle}</h2>
+{/if}
 
 <style>
 
