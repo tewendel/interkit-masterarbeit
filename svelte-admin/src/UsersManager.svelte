01c65de535c72931e073acebc22e4ed8e6a85dba
Jakob Wierzba <interkitgitlab@jakobwierzba.de>
2023-03-14 12:11:40 +0100

    improve users table buttons

--- svelte-admin/src/UsersManager.svelte
+++ svelte-admin/src/UsersManager.svelte
@@ -3,7 +3,12 @@
   import { onDestroy } from 'svelte';
   import { InterkitClient } from 'interkit'
   import UsersList from './UsersList.svelte'
-  import { Form, TextInput, Button } from "carbon-components-svelte"
+  import {
+    Form,
+    TextInput,
+    Button,
+    Modal
+  } from "carbon-components-svelte"
 
   export let projectId
   export let previewUserId
@@ -35,6 +40,8 @@
 
   onDestroy(()=> unsubscribe());
 
+  let openCreateNewUser = false
+
   const createNewUser = () => {
     const projectData = { createdInBackEnd: true }
     const data = { username, password, email, projectData, projectId }
@@ -51,17 +58,26 @@
   {updatePreviewUserAuth}
   {moveToBoardId}
   {moveToNodeId}
+  on:clickedAddUser={() => { openCreateNewUser = true }}
   />
 
-<br><br>
-
-<Form on:submit={createNewUser}>
-  <TextInput bind:value={username} inline light labelText="User name" />
-  <TextInput bind:value={email} inline light labelText="Email" />
-  <TextInput bind:value={password} inline light labelText="Password" />
-  <br>
-  <Button type="submit">Create New Project User</Button>
-</Form>
+<Modal
+  bind:open={openCreateNewUser}
+  modalHeading="Create new project user"
+  primaryButtonText="Create"
+  secondaryButtonText="Cancel"
+  on:click:button--secondary={() => { openCreateNewUser = false }}
+  on:submit={() => { createNewUser(); openCreateNewUser = false }}
+  shouldSubmitOnEnter={false}
+  >
+  <Form on:submit={createNewUser}>
+    <TextInput bind:value={username} inline light labelText="User name" />
+    <TextInput bind:value={email} inline light labelText="Email" />
+    <TextInput bind:value={password} inline light labelText="Password" />
+  </Form>
+  <!-- FIXME there is no error handling, e.g. if an email exists
+    you only get an error in the console -->
+</Modal>
 
 <!--pre>
 {JSON.stringify(usersArray, null, 2)}
