bdea306637db3d1504ad917ef2710ef9eec48d5f
Holger H <holgheiss@gmail.com>
2023-03-30 16:58:07 +0200

    [docs] vite migration

--- sites/docs/src/content/sidebar.json.js
+++ sites/docs/src/content/sidebar.json.js
@@ -44,6 +44,10 @@ export const items = [
 			{
 				title: 'Setup Raspberry Player',
 				path: '/guides/raspi_setup'
+			},
+			{
+				title: 'Migrate app to vite',
+				path: '/guides/migrate_to_vite'
 			}
 		]
 	},
