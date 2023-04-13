9bc2dd825ac293dde1bf39e8dd1dfe09257d9ad9
Holger H <holgheiss@gmail.com>
2023-04-13 17:06:41 +0200

    admin: put $currentProject in admin.js

--- svelte-admin/src/admin.js
+++ svelte-admin/src/admin.js
@@ -1,6 +1,41 @@
-import { writable } from 'svelte/store'
+import { writable, derived } from 'svelte/store'
+import {InterkitClient} from 'interkit'
 
 export const projectId = writable()
 
 export const secondaryTabIndex = writable(0);
 export const secondaryTabsVisible = writable(true);
+
+let subStatus = {}
+export const currentProject = derived(
+  projectId,
+  async ($projectId, set) => {
+
+    if(subStatus?.currentProjectSub?.stop && (subStatus.currentProjectId && subStatus.currentProjectId != $projectId || !$projectId)) {
+      console.log("unsubscribing project", subStatus.currentProjectId)
+      await subStatus.currentProjectSub.stop()
+      subStatus = {}
+      set(null)
+    }
+
+    if ($projectId) {
+      console.log("subscribing project", $projectId)
+
+      const currentProjectSub = await InterkitClient.getSub('projects', 'project', $projectId, (p)=>p.id == $projectId, true)
+
+      subStatus.currentProjectSub = currentProjectSub
+      subStatus.currentProjectId = $projectId
+
+      currentProjectSub.data?.subscribe((p)=>{
+        set(p)
+      })
+    }
+
+    return async () => {
+      if(subStatus?.currentProjectSub?.stop) {
+        await subStatus.currentProjectSub.stop()
+      }
+    };
+
+  }
+);
