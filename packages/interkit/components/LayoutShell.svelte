96f0b707d60fea35e0e3789d30ac75306edb6fe0
Sebastian Quack <Sebastian Quack>
2023-04-27 13:02:04 +0200

    layoutshell

---
+++ packages/interkit/components/LayoutShell.svelte
@@ -0,0 +1,67 @@
+<script>
+
+  
+</script>
+
+<div class="container">
+  {#if $$slots.TopBar}
+  <div class="top-bar">
+    <slot name="TopBar"></slot>
+  </div>
+  {/if}
+  <div class="content">
+    <div class="reset">
+      <slot name="Content"></slot>
+    </div>
+  </div>
+  {#if $$slots.BottomBar}
+  <div class="bottom-bar">
+    <slot name="BottomBar"></slot>
+  </div>
+  {/if}
+</div>
+
+<style>
+
+  .container {
+    flex-direction: column;
+    height: 100%;
+    display: flex;
+  }
+
+  .content, .top-bar, .bottom-bar {
+    display: flex;
+  }
+
+  .top-bar {
+    height: 64px;
+    border-bottom: 1px solid gray;
+
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+  }
+
+  .bottom-bar {
+    height: 64px;
+    border-top: 1px solid gray;
+
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+  }
+
+  .content {
+    flex: 1;
+    overflow-y: scroll;
+  }
+
+  .reset {
+    display: block;
+    width: 100%;
+    height: 100%;
+  }
+
+  
+
+</style>
