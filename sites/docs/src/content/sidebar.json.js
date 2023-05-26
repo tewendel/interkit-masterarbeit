b01d4eeb50221fadf1f76b275a64593a702d91c3
Jakob Wierzba <interkitgitlab@jakobwierzba.de>
2023-05-26 14:45:18 +0200

    spaces, not tabs

--- sites/docs/src/content/sidebar.json.js
+++ sites/docs/src/content/sidebar.json.js
@@ -1,152 +1,152 @@
 export const items = [
-	{
-		title: 'Basics',
-		open: true,
-		items: [
-			{
-				title: 'Introduction',
-				path: '/'
-			},
-			{
-				title: 'Interface Overview',
-				path: '/interface_overview'
-			},
-			{
-				title: 'How to start',
-				path: '/basics/build_app'
-			},
-			{
-				title: 'Elements',
-				path: '/basics/elements'
-			}
-		]
-	},
-	{
-		title: 'Guides',
-		open: false,
-		items: [
-			{
-				title: 'How to setup your own interkit server',
-				path: '/guides/server_setup'
-			},
-			{
-				title: 'Setup push notifications',
-				path: '/guides/push_setup'
-			},
-			{
-				title: 'Setup server cron jobs',
-				path: '/guides/cron_setup'
-			},
-			{
-				title: 'Internationalization (i18n) and localization (l10n)',
-				path: '/guides/i18n'
-			},
-			{
-				title: 'Build for native app and deploy to app stores',
-				path: '/guides/native'
-			},
-			{
-				title: 'Setup Raspberry Player',
-				path: '/guides/raspi_setup'
-			},
-			{
-				title: 'Migrate app to vite',
-				path: '/guides/migrate_to_vite'
-			},
-			{
-				title: 'Story programming cheatsheet',
-				path: '/guides/story_cheatsheet'
-			}
-		]
-	},
-	{
-		title: 'Components',
-		open: false,
-		items: [
-			{
-				title: 'BottomMenu',
-				path: '/components/BottomMenu'
-			},
-			{
-				title: 'Button',
-				path: '/components/Button'
-			},
-			{
-				title: 'MapSimple',
-				path: '/components/MapSimple'
-			},
-			{
-				title: 'ElementList',
-				path: '/components/ElementList'
-			},
-			{
-				title: 'ContentElement',
-				path: '/components/ContentElement'
-			},
-			{
-				title: 'ElementsContextProvider',
-				path: '/components/ElementsContextProvider'
-			},
-			{
-				title: 'ElementProvider',
-				path: '/components/ElementProvider'
-			},
-			{
-				title: 'Chat',
-				path: '/components/Chat'
-			},
-			{
-				title: 'SubsectionsNav',
-				path: '/components/SubsectionsNav'
-			},
-			{
-				title: 'Subsection',
-				path: '/components/Subsection'
-			},
-			{
-				title: 'actions.js',
-				path: '/components/actions'
-			}
-		]
-	},
-	{
-		title: 'Theory',
-		open: false,
-		items: [
-			{
-				title: 'Collection Schemas',
-				path: '/theory/collections'
-			},
-			{
-				title: '"Project Server"',
-				path: '/theory/project-server'
-			},
-			{
-				title: 'Server Architecture',
-				path: '/theory/server-architecture'
-			}
-		]
-	},
-	{
-		title: 'Contribute',
-		open: false,
-		items: [
-			{
-				title: 'Docs',
-				path: '/contribute/docs'
-			},
-			{
-				title: 'Components',
-				path: '/contribute/components'
-			},
-			{
-				title: 'Component CSS',
-				path: '/contribute/component_css'
-			},
-			{
-				title: 'Custom Theme',
-				path: '/contribute/theme'
-			}
-		]
-	}
+  {
+    title: 'Basics',
+    open: true,
+    items: [
+      {
+        title: 'Introduction',
+        path: '/'
+      },
+      {
+        title: 'Interface Overview',
+        path: '/interface_overview'
+      },
+      {
+        title: 'How to start',
+        path: '/basics/build_app'
+      },
+      {
+        title: 'Elements',
+        path: '/basics/elements'
+      }
+    ]
+  },
+  {
+    title: 'Guides',
+    open: false,
+    items: [
+      {
+        title: 'How to setup your own interkit server',
+        path: '/guides/server_setup'
+      },
+      {
+        title: 'Setup push notifications',
+        path: '/guides/push_setup'
+      },
+      {
+        title: 'Setup server cron jobs',
+        path: '/guides/cron_setup'
+      },
+      {
+        title: 'Internationalization (i18n) and localization (l10n)',
+        path: '/guides/i18n'
+      },
+      {
+        title: 'Build for native app and deploy to app stores',
+        path: '/guides/native'
+      },
+      {
+        title: 'Setup Raspberry Player',
+        path: '/guides/raspi_setup'
+      },
+      {
+        title: 'Migrate app to vite',
+        path: '/guides/migrate_to_vite'
+      },
+      {
+        title: 'Story programming cheatsheet',
+        path: '/guides/story_cheatsheet'
+      }
+    ]
+  },
+  {
+    title: 'Components',
+    open: false,
+    items: [
+      {
+        title: 'BottomMenu',
+        path: '/components/BottomMenu'
+      },
+      {
+        title: 'Button',
+        path: '/components/Button'
+      },
+      {
+        title: 'MapSimple',
+        path: '/components/MapSimple'
+      },
+      {
+        title: 'ElementList',
+        path: '/components/ElementList'
+      },
+      {
+        title: 'ContentElement',
+        path: '/components/ContentElement'
+      },
+      {
+        title: 'ElementsContextProvider',
+        path: '/components/ElementsContextProvider'
+      },
+      {
+        title: 'ElementProvider',
+        path: '/components/ElementProvider'
+      },
+      {
+        title: 'Chat',
+        path: '/components/Chat'
+      },
+      {
+        title: 'SubsectionsNav',
+        path: '/components/SubsectionsNav'
+      },
+      {
+        title: 'Subsection',
+        path: '/components/Subsection'
+      },
+      {
+        title: 'actions.js',
+        path: '/components/actions'
+      }
+    ]
+  },
+  {
+    title: 'Theory',
+    open: false,
+    items: [
+      {
+        title: 'Collection Schemas',
+        path: '/theory/collections'
+      },
+      {
+        title: '"Project Server"',
+        path: '/theory/project-server'
+      },
+      {
+        title: 'Server Architecture',
+        path: '/theory/server-architecture'
+      }
+    ]
+  },
+  {
+    title: 'Contribute',
+    open: false,
+    items: [
+      {
+        title: 'Docs',
+        path: '/contribute/docs'
+      },
+      {
+        title: 'Components',
+        path: '/contribute/components'
+      },
+      {
+        title: 'Component CSS',
+        path: '/contribute/component_css'
+      },
+      {
+        title: 'Custom Theme',
+        path: '/contribute/theme'
+      }
+    ]
+  }
 ];
