efb10d930f58a250e3ab81cedc072d8824c1062e
Holger H <holgheiss@gmail.com>
2023-06-17 18:20:57 +0200

    make style editor work

--- svelte-admin/src/Style/StyleEditor.svelte
+++ svelte-admin/src/Style/StyleEditor.svelte
@@ -1,12 +1,12 @@
 <script>
   import StyleTokensForm from '../InputModals/StyleTokensForm.svelte'
 
-  export let currentStyleTokens = null
+  export let currentStyleTokens = {}
 
   const update = (e) => {
-    console.log("StyleTokensForm dispatch", e.detail);
-    currentStyleTokens = e.detail
+    //console.log("StyleTokensForm dispatch", e.detail);
+    //currentStyleTokens = e.detail
   }
 </script>
 
-<StyleTokensForm on:update={update} bind:value={currentStyleTokens}/>
+
--- svelte-admin/src/Style/Theming.svelte
+++ svelte-admin/src/Style/Theming.svelte
@@ -1,23 +1,51 @@
 <script>
-  import { ButtonSet, Button } from 'carbon-components-svelte'
+  import { ButtonSet, Button, InlineNotification } from 'carbon-components-svelte'
   import { Help, Reset, Save, TrashCan } from 'carbon-icons-svelte'
   import MainColumns from '../Layout/MainColumns.svelte'
   import ComponentsShowcase from './ComponentsShowcase.svelte'
   import StyleEditor from './StyleEditor.svelte'
+  import StyleTokensForm from '../InputModals/StyleTokensForm.svelte'
   import { docsGo } from '../docs.js'
   import { BundleServer } from '../BundleServer.js'
+  import { projectId, currentProject } from '../admin.js'
 
-  let currentStyleTokens = null
+  let originalStyleTokens = $currentProject.uiState.styleTokens
+  let modified = false
+  let equalsDefaults = true
+  let currentStyleTokens = {...originalStyleTokens}
 
   const save = async () => {
     console.log("save", currentStyleTokens)
-    //await BundleServer.saveStyleTokens(projectId, currentStyleTokens)
+    await BundleServer.saveSrcFile({
+      projectId: $projectId,
+      file: {
+        filename: 'styleTokens.json',
+        content: JSON.stringify(currentStyleTokens)
+      }
+    })
+    originalStyleTokens = {...currentStyleTokens}
+  }
+
+  const trash = () => {
+    currentStyleTokens = {}
+    save()
   }
 
   const reset = () => {
-    currentStyleTokens = null
+    currentStyleTokens = originalStyleTokens
   }
 
+  const equals = (a, b) => {
+    return JSON.stringify(a) === JSON.stringify(b)
+  }
+
+  // update style tokens from project if they differ
+  $: if (!equals($currentProject.uiState.styleTokens,originalStyleTokens)) originalStyleTokens = $currentProject.uiState.styleTokens
+  // true when the project doesn't define it's own style tokens
+  $: originalStyleIsEmpty = !originalStyleTokens || equals(originalStyleTokens,{})
+  // true when there is something to save or to undo
+  $: modified = !equals(currentStyleTokens, originalStyleTokens) && !equalsDefaults
+  
 </script>
 
 <MainColumns
@@ -41,17 +69,20 @@
             <Button
               kind="ghost"
               icon={TrashCan}
-              iconDescription="Reset Changes"
+              disabled={originalStyleIsEmpty}
+              on:click={trash}
+              iconDescription="Reset project to default style"
               />
             <Button
               kind="ghost"
+              disabled={!modified}
               icon={Reset}
               on:click={reset}
-              iconDescription="Reset Changes"
+              iconDescription="Undo Changes"
               />
             <Button
               icon={Save}
-              disabled={!currentStyleTokens}
+              disabled={!modified}
               on:click={save}
               >
               Save
@@ -60,7 +91,32 @@
       </div>
       <div class="main-content">
 
-        <StyleEditor bind:currentStyleTokens />
+        {#if originalStyleIsEmpty}
+          <InlineNotification
+            hideCloseButton
+            kind="info-square"
+            title="Default Style"
+            subtitle="This project uses the default interkit style"
+          />
+        {:else}
+          {#if equalsDefaults}
+            <InlineNotification
+              hideCloseButton
+              kind="info-square"
+              title="Default Style"
+              subtitle="This project uses it's own styles, but they are equal to the interkit defaults"
+            />
+          {:else}
+            <InlineNotification
+              hideCloseButton
+              kind="success"
+              title="Custom Style"
+              subtitle="This project uses styles that differ from the interkit defaults"
+            />
+          {/if}
+        {/if}
+
+        <StyleTokensForm bind:value={currentStyleTokens} bind:equalsDefaults={equalsDefaults} />
         <ComponentsShowcase {currentStyleTokens} />
       </div>
 
