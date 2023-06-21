421d876f8ad1a8f7b134688740c3383bf88606b6
Sebastian Quack <Sebastian Quack>
2023-06-21 16:53:28 +0200

    add listItem

---
+++ packages/interkit/components/ListItem.svelte
@@ -0,0 +1,44 @@
+<script>
+
+  import WithEffect from './WithEffect.svelte';
+  export let text;
+  export let effect;
+  
+</script>
+
+<WithEffect {effect} let:execute>
+  <!-- svelte-ignore a11y-click-events-have-key-events -->
+  <div class="container {effect ? "withEffect" : ""}" on:click={execute}>
+    <span class="text">{text}</span>
+    <div class="icon-container">
+      <slot/>
+    </div>
+  </div>
+</WithEffect>
+
+<style>
+
+  .container {
+    width: 100%;
+    border-bottom: var(--border-width) solid var(--color-border);
+    padding: 12px 16px 12px 16px;
+    display: flex;
+    box-sizing: border-box;
+  }
+
+  .container.withEffect:hover {
+    cursor: pointer;
+  }
+
+  .text {
+    font: var(--font-subtitle-1);
+    letter-spacing: var(--letter-spacing-subtitle-1);
+    flex: 1;
+  }
+
+  .icon-container {
+    justify-self: right;
+    align-self: flex-end;
+  }
+
+</style>
