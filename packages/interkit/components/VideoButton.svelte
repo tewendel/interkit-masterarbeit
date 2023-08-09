e5a336e5d8fb35b21f13c91639da0593e1482325
Sebastian Quack <Sebastian Quack>
2023-08-09 10:00:20 +0200

    add custom controls to video player

--- packages/interkit/components/VideoButton.svelte
+++ packages/interkit/components/VideoButton.svelte
@@ -1,10 +1,13 @@
 <script>
 
-  import { getContext } from 'svelte'
+  import { getContext, onMount, onDestroy } from 'svelte'
+  import { fly } from 'svelte/transition'
   import Button from "./Button.svelte"
   import Icon from './Icon.svelte';
   import MediaFileResolver from './MediaFileResolver.svelte';
   import Overlay from './Overlay.svelte';
+  import Loading from './Loading.svelte'
+  
   import { util } from '../'
 
   export let videoColumn
@@ -30,11 +33,72 @@
   }
   
 
-  let playerOpen = false;
+  let playerOpen = false
+  let showControls = true
 
-  const openPlayer = () => playerOpen = true;
-  const closePlayer = () => playerOpen = false;
-    
+  const openPlayer = () => {
+    playerOpen = true
+    showControls = true
+  }
+  const closePlayer = () => playerOpen = false
+
+  let rangeSliderValue;
+  let rangeSliderDragging;
+  let videoPlayerDuration;
+  let videoPlayerCurrentTime;
+  let videoPlayerLoading;
+  let videoPlayerPaused;
+
+  const format = (seconds) => {
+    if (isNaN(seconds)) return '...';
+    const minutes = Math.floor(seconds / 60);
+    seconds = Math.floor(seconds % 60);
+    if (seconds < 10) seconds = '0' + seconds;
+    return `${minutes}:${seconds}`;
+  }
+
+  // hide controls automatically
+  let hideControlsTimeout
+  const resetControlsTimeout = () => {
+    console.log("resetControlsTimeout")
+    if(hideControlsTimeout) clearTimeout(hideControlsTimeout)
+    hideControlsTimeout = setTimeout(()=>{
+      if(!videoPlayerPaused)
+        showControls = false;
+    }, 10000)
+  }
+  onMount(()=>{
+    resetControlsTimeout()
+  })
+  onDestroy(()=>{
+    clearTimeout(hideControlsTimeout)
+  })
+  
+  const toggleControls = () => {
+    if(controls == "interkit")
+      showControls = !showControls
+    if(showControls) resetControlsTimeout();
+  }
+  
+  const seek = (seconds) => {
+    videoPlayerCurrentTime += seconds
+    resetControlsTimeout()
+  }
+  const seekTo = (seconds) => {
+    videoPlayerCurrentTime = seconds
+    resetControlsTimeout()
+  }
+  const togglePlay = () => {    
+    videoPlayerPaused = !videoPlayerPaused;
+    videoPlayerCurrentTime = videoPlayerCurrentTime == videoPlayerDuration ? 0 : videoPlayerCurrentTime
+    resetControlsTimeout()
+  }
+  const updateSliderValue = (value) => {
+    if(!rangeSliderDragging) {
+      rangeSliderValue = value
+    }
+  }
+  $: updateSliderValue(videoPlayerCurrentTime)
 
 </script>
 
@@ -49,16 +113,77 @@
     </div>
 
     <!-- svelte-ignore a11y-media-has-caption -->
-    <video autoplay controls={controls == "native"} {loop}>
+    <video 
+      autoplay 
+      controls={controls == "native"} 
+      {loop} 
+      on:click={toggleControls}
+      bind:currentTime={videoPlayerCurrentTime}
+      bind:duration={videoPlayerDuration}
+      bind:paused={videoPlayerPaused}
+    >
       <MediaFileResolver let:url mediafileRef={videoFileRef} >
         <source src={url} >
       </MediaFileResolver>
     </video>
 
-    {#if controls == "interkit"}
+    {#if controls == "interkit" && showControls}
 
-    
+    <div class="expanded-bottom-bar AudioPlayer__BottomBar" out:fly|local={{ y: 200, duration: 500 }}>
 
+      <div class="expanded-range-slider AudioPlayer__Range expanded">
+        <input 
+          type="range" 
+          class="seekPositionRangeSlider AudioPlayer__Range__Input" 
+          name="seekPosition"
+          min="0" 
+          step="0.05"
+          max={videoPlayerDuration} 
+          bind:value={rangeSliderValue}
+          on:input={()=>{rangeSliderDragging = true;}}
+          on:change={()=>{
+            seekTo(rangeSliderValue)
+            rangeSliderDragging = false;
+          }}
+        >
+      </div>
+
+      <div class="expanded-time AudioPlayer__Time expanded">
+        <span class="currentTime">{format(videoPlayerCurrentTime)}</span>
+        <span class="duration">{format(videoPlayerDuration)}</span> 
+      </div>
+
+      <div class="expanded-controls AudioPlayer__Controls">
+
+        <div class="center-controls AudioPlayer__CenterControls">
+
+          <div class="AudioPlayer__PlayButton seekbutton">
+            <Button dummyNoText size="small" type="link" on:click={()=>{seek(-30)}}>
+              <Icon type="Thin-Replay-30" />
+            </Button>
+          </div>
+
+          <div class="AudioPlayer__PlayButton playbutton">
+            {#if videoPlayerLoading }
+              <Loading inverse/>
+            {:else}
+              <Button dummyNoText size="large" type="primary" on:click={togglePlay}>
+                <Icon inverse type={ videoPlayerPaused ? "Full-Play" : "Full-Pause"} />
+              </Button>
+            {/if}
+          </div>
+
+          <div class="AudioPlayer__PlayButton seekbutton">
+            <Button dummyNoText size="small" type="link" on:click={()=>{seek(15)}}>
+              <Icon type={"Thin-Forward-15"} />
+            </Button>
+          </div>
+
+        </div>
+        
+      </div>
+
+    </div>      
 
     {/if}
 
@@ -83,6 +208,78 @@
   video {
     width: 100%;
     height: 100%;
+  }
+
+  .expanded-bottom-bar {
+    background-color: var(--color-background-highlight);
+    padding: 16px 8px;
+    box-sizing: border-box; 
+    position: absolute;
+    width: 100%;
+    bottom: 0;
+  }
+
+  .expanded-controls, .expanded-range-slider, .expanded-time {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    box-sizing: border-box;
+    padding: 4px 8px 0 8px;
+  }
+
+  .expanded-controls .center-controls {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    gap: 25px;
+    flex: 1;
+  }
+
+  .expanded-time {
+    margin-top: 8px;
+    margin-bottom: 8px;
+  }
+
+  .expanded-time span {
+    font: var(--font-caption);
+    letter-spacing: var(--letter-spacing-caption);
+  }
+
+  :not(.expanded) .base-content .seekbutton {
+    display: none;
+  }
+  
+  .seekPositionRangeSlider {
+    width: 100%;
+    -webkit-appearance: none;
+    background-color: var(--color-border-button-primary);
+    height: 1px;
+    margin: 0;
+    /* this is to have a larger target to tap */
+    /* border-top: 10px solid #000; 
+    border-bottom: 10px solid #000; */
+  }
+
+  .seekPositionRangeSlider::-moz-range-thumb {
+    -webkit-appearance: none;
+    height: 14px;
+    width: 14px;
+    background: var(--color-border-button-primary);
+    margin-top: -5px;
+    border-radius: 50%;
+    border: none;
+  }
+  
+  .seekPositionRangeSlider::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    height: 14px;
+    width: 14px;
+    background: var(--color-border-button-primary);
+    border-radius: 50%;
+    border: none;
   }
 
 </style>
