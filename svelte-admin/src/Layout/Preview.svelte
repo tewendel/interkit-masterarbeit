4970582f7ca50dc3bec4ce25be4ac4ed3e6bdc4d
Jakob Wierzba <interkitgitlab@jakobwierzba.de>
2023-08-24 08:12:41 +0200

    defang inter-frame comms error alert→warn #446

--- svelte-admin/src/Layout/Preview.svelte
+++ svelte-admin/src/Layout/Preview.svelte
@@ -135,13 +135,10 @@
     }
   }
 
-  let didAlertPostMessage = false
   let postMessageOrigin
   if (document.location.port) {
     console.warn('assuming dev mode, allowing unsafe inter-frame communication')
     postMessageOrigin = '*'
-    // we also silence the alert, due to false positives during HMR
-    didAlertPostMessage = true
   } else {
     postMessageOrigin = document.location.origin
   }
@@ -158,8 +155,7 @@
         iframeRef.contentWindow.postMessage(commandOrObj, postMessageOrigin) 
       }
     } catch (e) {
-      (didAlertPostMessage ? console.warn : window.alert)(`Inter-frame communication with the preview failed. Check your script blockers and see docs: Basics/Interface Overview, Troubleshooting`)
-      didAlertPostMessage = true
+      console.warn(`Inter-frame communication with the preview failed. Check your script blockers and see docs: Basics/Interface Overview, Troubleshooting`)
     }
   }
 
