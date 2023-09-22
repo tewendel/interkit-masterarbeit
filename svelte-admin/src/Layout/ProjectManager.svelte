70dcf089ae36350eac15195adca0fe3c07d24eb1
Sebastian Quack <Sebastian Quack>
2023-09-22 15:02:19 +0200

    separate project wizard and list

--- svelte-admin/src/Layout/ProjectManager.svelte
+++ svelte-admin/src/Layout/ProjectManager.svelte
@@ -1,66 +1,41 @@
 <script>
-  import { push } from 'svelte-spa-router';
-  import { onDestroy } from 'svelte'
+  import { onDestroy } from 'svelte'  
   
   import { InterkitClient } from 'interkit'
-  import { bundleServerURL$ } from '../BundleServer.js'
-
-  import ProjectWorkspace from './ProjectWorkspace.svelte'
-  import ProjectCreationWizard from './ProjectCreationWizard.svelte'
-  import SecondaryTabsContent from "./SecondaryTabsContent.svelte"
-
-  import DataTablePaginationAutofit from '../Data/DataTablePaginationAutofit.svelte'
-
-  import { 
-    Grid,
-    Row,
-    Column,
-    Loading,
-    DataTable,
-    Toolbar,
-    ToolbarContent,
-    Button,
-    ButtonSet,
-    Tag,
-    OverflowMenu,
-    OverflowMenuItem,
-    ImageLoader
-  } from "carbon-components-svelte";
-
-  import Help from 'carbon-icons-svelte/lib/Help.svelte'
-  import Information from 'carbon-icons-svelte/lib/Information.svelte'
-  import Add from 'carbon-icons-svelte/lib/Add.svelte'
-  import TrashCan from "carbon-icons-svelte/lib/TrashCan.svelte";
-  import Copy from "carbon-icons-svelte/lib/Copy.svelte";
-  import Edit from "carbon-icons-svelte/lib/Edit.svelte";
-  import QID from 'carbon-icons-svelte/lib/QID.svelte'
-  import Template from 'carbon-icons-svelte/lib/Template.svelte'
-  import WatsonHealthThumbnailPreview from 'carbon-icons-svelte/lib/WatsonHealthThumbnailPreview.svelte'
   import {
     currentProject,
     projectId,
     secondaryTabIndex,
-    secondaryTabSpecialDoc,
     secondaryTabPreviewProjectId,
     secondaryTabsSizes,
     secondaryTabsSize
   } from '../admin.js'
-
   import { docsGo } from '../docs.js'
 
+  import ProjectWorkspace from './ProjectWorkspace.svelte'
+  import ProjectList from './ProjectList.svelte'
+  import ProjectCreationWizard from './ProjectCreationWizard.svelte'
+  import SecondaryTabsContent from "./SecondaryTabsContent.svelte"
+  
+  import {
+    Button, 
+    Grid,
+    Row,
+    Column,
+    Loading,
+    Tabs,
+    Tab,
+    TabContent
+  } from "carbon-components-svelte";
+  import Help from 'carbon-icons-svelte/lib/Help.svelte'
+  import Add from 'carbon-icons-svelte/lib/Add.svelte'
+  
   export let params = {} // these come from the route
 
-  let createProjectStep = false
-  
-  let userIsRole = InterkitClient.userIsRole
-
   let projectsListSub;
-
-  let previewUserAuth;
-
   let projects;
-
-  const bundleServerURL = bundleServerURL$
+  let previewUserAuth;
+  let createProjectStep = false
 
   const manageProjectsSub = async (_projectId)=>{
     console.log("project subscription " + _projectId)
@@ -78,109 +53,43 @@
     projectsListSub?.stop()
   })
 
-  //$: currentProjectId = $projectId
-  $: tab = params.tab
-
   $: manageProjectsSub($projectId)
 
-  // add "id" for carbon table
+  $: tab = params.tab // comes from the route
+
+  // get the created date from the history
+  const getCreatedDate = project => {
+    return (project?.history || []).find(h => h.event == "create_project")?.date
+  }
+
+  // just the projects, no templates, add "id" for carbon table
   $: projectRows = projects
     ? $projects
+      .filter(p => !p.isTemplate)  
       .map(p => ({ ...p, id: p.id, createdAt: getCreatedDate(p) }))
-      // only admins can see templates in the project table
-      .filter(p => $userIsRole?.admin === true || !p.isTemplate)
       .sort((p1, p2) => p1 - p2)
     : []
-
-  let expandedRowIds = []
-  $: expandedRowIds = projectRows
-    .filter(row => row.uiState?.metafile?.description?.html)
-    .map(row => row.id)
-
-  let nonExpandableRowIds = []
-  $: nonExpandableRowIds = projectRows
-    .filter(row => !row.uiState?.metafile?.description?.html)
-    .map(row => row.id)
-
+  
+  // just the templates, add "id" for carbon table
   let templates
   $: templates = projects
-    ? $projects.filter(_ => _.isTemplate)
+    ? $projects
+        .filter(_ => _.isTemplate)
         .map(p => ({ ...p, id: p.id, createdAt: getCreatedDate(p) }))
         .sort((a, b) => a.name < b.name ? -1 : 1)
     : []
 
+  // find the starter template  
   let createProjectEmptyTemplate
   $: createProjectEmptyTemplate = projectRows
     .filter(_ => _.isTemplate && (_.slug === 'empty' || _.name === 'empty'))?.[0]
-
-  const openProject = id => push('/' + id)
-
-  const infoProject = (row, file) => {
-    secondaryTabIndex.set(1)
-    secondaryTabSpecialDoc.set(row.uiState.metafile[file].html)
-  }
-
+  
+  // show a project in the preview pane
   const previewProject = id => {
     secondaryTabIndex.set(0)
     secondaryTabPreviewProjectId.set(id)
   }
     
-  const removeProject = async (projectId) => {
-    if(confirm("really delete project?")) {
-      await InterkitClient.call("project.remove", {projectId})
-    }
-  }
-
-  const renameProject = async (row) => {
-    let newName = prompt("Projekt umbenennen", row.name)
-    if(newName) {
-      await InterkitClient.call("project.rename", {projectId: row._id, newName});
-    }
-  }
-
-  const duplicateProject = async (row) => {
-    console.log("duplicating project", row.id)
-    const newProjectId = await InterkitClient.call("project.duplicate", {projectId: row.id})
-  }
-
-  const updateProjectSetIsTemplate = async (row, isTemplate) => {
-    await InterkitClient.call('project.setIsTemplate', { projectId: row._id, isTemplate })
-  }
-
-  const getCreatedDate = project => {
-    return (project?.history || []).find(h => h.event == "create_project")?.date
-  }
-
-  import { projectManagerSortKey, projectManagerSortDirection, projectManagerPage } from '../admin.js'
-  
-  let sortKey = $projectManagerSortKey
-  $: projectManagerSortKey.set(sortKey)
-
-  let sortDirection = $projectManagerSortDirection
-  $: projectManagerSortDirection.set(sortDirection)
-
-  let page = 1
-  $: projectManagerPage.set(page)
-
-  let pageSize = 10
-  
-  const dataTableOverheadHeight = 0 +
-    48 + // header of outer UI
-    68 + // DataTable title
-    2 + // DataTable container padding top
-    48 + // DataTable toolbar
-    48 + // DataTable thead = 1 row height
-    40 + // DataTable tfoot
-    24   // potential horizontal scrollbar + buffer
-
-  const headers = [
-    { key: 'favicon', empty: true },
-    { key: 'name', value: 'Project name' },
-    { key: 'createdAt', value: 'Created At', sort: (a, b) => new Date(a||0) - new Date(b||0), },
-    { key: 'cpu', value: 'CPU usage' },
-    { key: 'overflow', empty: true }
-  ]
-
 </script>
 
 <Grid style="padding:0; height:100%; overflow-x: hidden; overflow-y: auto; max-width: none;">
@@ -207,159 +116,53 @@
               <Loading style="background-color:white"/>
             {/if}
           {:else if createProjectStep === false}
-            <DataTable
-              style={`
-                background: #f4f4f4;
-                /* = pageSize * row + search/actions + thead + data table padding-top */
-                min-height: ${(pageSize || 0) * 48 + 32 + 48 + 2 + 68}px;
-              `}
-              title="Projects on this server"
-              sortable
-              bind:sortKey
-              bind:sortDirection
-              {pageSize}
-              bind:page
-              {headers}
-              rows={projectRows}
-              batchExpansion
-              
-              {nonExpandableRowIds}
-              >
-              <Toolbar>
-                <ToolbarContent>
-                  <Button
-                    size="small"
-                    kind="ghost"
-                    icon={Help}
-                    on:click={() => docsGo('/basics/build_app#creating-a-project')}
+            <Tabs>
+              <Tab label="Your Projects" />
+              <Tab label="Templates" />
+              <svelte:fragment slot="content">
+                <div class="__ProjectList">
+                  <TabContent>
+                    <ProjectList 
+                      {projectRows}
+                      {previewProject}
+                      description="These are the projects created by your team on this server."
                     >
-                    Help
-                  </Button>
-                  <Button
-                    size="small"
-                    icon={Add}
-                    on:click={() => { createProjectStep = 0 }}
+                      <Button
+                        size="small"
+                        kind="ghost"
+                        icon={Help}
+                        on:click={() => docsGo('/basics/build_app#creating-a-project')}
+                        >
+                        Help
+                      </Button>
+                      <Button
+                        size="small"
+                        icon={Add}
+                        on:click={() => { createProjectStep = 0 }}
+                        >
+                        Create project…
+                      </Button>
+                    </ProjectList>
+                  </TabContent>
+                  <TabContent>
+                    <ProjectList 
+                      projectRows={templates}
+                      description="These are the templates available on this server as starting points."
+                      {previewProject}
                     >
-                    Create project…
-                  </Button>
-                </ToolbarContent>
-              </Toolbar>
-              <span slot="cell" let:row let:cell>
-                {#if cell.key === 'favicon'}
-                  {#if $bundleServerURL}
-                    <ImageLoader
-                      src={`${$bundleServerURL}/app/${row.id}/favicon.png`}
-                      alt="Favicon"
-                      fadeIn
-                      style="max-width: 2em"
-                      >
-                      <svelte:fragment slot="error">
-                      </svelte:fragment>
-                    </ImageLoader>
-                  {/if}
-                {/if}
-                {#if cell.key === 'name'}
-                  <!--<span on:click={() => previewProject(row.id)} class="clickable">-->
-                  <span class="name-field">
-                    {#if row.isTemplate}
-                      <Tag>Template</Tag>
-                    {/if}
-                    {row.name}
-                    {#if row.uiState?.metafile?.description?.html}
-                      <br>
-                      <span class="project-description">
-                        {@html row.uiState?.metafile?.description?.html}
-                      </span>
-                    {/if}
-                  </span>
-                {/if}
-                {#if cell.key === 'createdAt'}
-                  <span class="soft">
-                    {#if row.createdAt}
-                      {row.createdAt.toLocaleDateString('de-DE', { year: 'numeric', month: 'short', day: 'numeric' })}
-                    {:else}
-                      -
-                    {/if}
-                  </span>
-                {/if}
-                {#if cell.key === 'cpu'}
-                  {#if row.projectServer?.status === 'running' }
-                    { (100 * row.projectServer?.cpu).toFixed(2) }%
-                  {:else}
-                    {row.projectServer?.status || ''}
-                  {/if}
-                {/if}
-                {#if cell.key === 'overflow'}
-                  <ButtonSet style="justify-content: end">
-                    <Button
-                      kind="ghost"
-                      size="small"
-                      icon={Edit}
-                      on:click={() => openProject(row.id)}
-                      >Edit</Button>
-                    <!--
-                    <Button
-                      kind="ghost"
-                      size="small"
-                      icon={Information}
-                      disabled={!row.uiState?.metafile?.readme?.html}
-                      on:click={() => infoProject(row)}
-                      >Readme</Button>
-                    -->
-                    <Button
-                      kind="ghost"
-                      size="small"
-                      icon={WatsonHealthThumbnailPreview}
-                      on:click={() => previewProject(row.id)} 
-                      >Preview</Button>
-                    <OverflowMenu flipped>
-                      <OverflowMenuItem
-                        on:click={() => duplicateProject(row)}
-                        ><Copy />&ensp;Duplicate</OverflowMenuItem>
-                      <OverflowMenuItem
-                        on:click={() => renameProject(row)}
-                        ><QID />&ensp;Rename</OverflowMenuItem>
-                      <OverflowMenuItem
-                        danger
-                        on:click={() => removeProject(row.id)}
-                        ><TrashCan />&ensp;Delete</OverflowMenuItem>
-                      <OverflowMenuItem
-                        hasDivider
-                        on:click={() => infoProject(row, 'readme')}
-                        disabled={!row.uiState?.metafile?.readme?.html}
-                        ><Information />&ensp;Readme.md</OverflowMenuItem>
-                      <OverflowMenuItem
-                        on:click={() => infoProject(row, 'project')}
-                        disabled={!row.uiState?.metafile?.project?.html}
-                        ><Information />&ensp;Project.md</OverflowMenuItem>
-                      {#if $userIsRole?.admin}
-                        {#if row.isTemplate}
-                          <OverflowMenuItem
-                            hasDivider
-                            danger
-                            on:click={() => updateProjectSetIsTemplate(row, false)}
-                            ><Template />&ensp;Unset&nbsp;template</OverflowMenuItem>
-                        {:else}
-                          <OverflowMenuItem
-                            hasDivider
-                            danger
-                            on:click={() => updateProjectSetIsTemplate(row, true)}
-                            ><Template />&ensp;Set&nbsp;template</OverflowMenuItem>
-                        {/if}
-                      {/if}
-                    </OverflowMenu>
-                  </ButtonSet>
-                {/if}
-              </span>
-            </DataTable>
-            <DataTablePaginationAutofit
-              bind:pageSize
-              bind:page
-              totalItems={projectRows.length}
-              overheadHeight={dataTableOverheadHeight}
-              rowHeight={72}
-              pageSizeAuto={true}
-              />
+                      <Button
+                          size="small"
+                          kind="ghost"
+                          icon={Help}
+                          on:click={() => docsGo('/basics/build_app#creating-a-project')}
+                          >
+                          Help
+                        </Button>
+                    </ProjectList>
+                  </TabContent>   
+                </div> 
+              </svelte:fragment>
+            </Tabs>
           {:else if createProjectStep !== false}
             <ProjectCreationWizard 
               bind:createProjectStep 
@@ -408,20 +211,6 @@
     max-height: 72px;
   }
 
-  .project-description {
-    display: inline-block;
-  }
-
-  :global(.project-description p) {
-    font-size: 0.75rem;
-    line-height: 1rem;
-    margin-bottom: 0.25rem;
-  }
-
-  .project-description br {
-    display: none;
-  }
-
   .panes {
     display: flex;
     height: 100%;
@@ -444,6 +233,10 @@
   }
   .left-pane--has-current-project > :global(div:not(.active)) {
     display: none;
+  }
+
+  :global(.__ProjectList .bx--tab-content) {
+    padding: 0;
   }
 
   :global(.__ProjectWorkspace .bx--tile) {
