c7af1ad0e610bb963641f495c4a1e1a4db3fac68
Sebastian Quack <Sebastian Quack>
2023-09-22 18:10:41 +0200

    update routing in admin to allow for projects and templates

---
+++ sites/docs/src/components/TemplateLoader.svelte
@@ -0,0 +1,20 @@
+<script>
+
+  export let slug
+
+  function pageInIframe() {
+   return (window.location !== window.parent.location);
+  }
+
+  const open = () => {
+    if(pageInIframe()) {
+      window.top.postMessage({method: "open-template", slug}, '*')
+    } else {
+      alert("Opening templates with these buttons currently only works when viewing the docs inside the authoring system.")
+    }
+    
+  }
+
+</script>
+
+<button on:click={open}>Open template '{slug}'</button>
