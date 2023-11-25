bda3e14bae6bc6ff9d44aefe307fe8855863c6a0
Holger H <holgheiss@gmail.com>
2023-11-25 12:22:27 +0100

    admin refactor: create ProjectDashboard component

--- svelte-admin/src/Layout/ProjectWorkspace.svelte
+++ svelte-admin/src/Layout/ProjectWorkspace.svelte
@@ -1,14 +1,9 @@
 <script>
-  import {
-    Button,
-    ButtonSet,
-    Toggle,
-    Tag
-  } from "carbon-components-svelte";
-  import Copy from "carbon-icons-svelte/lib/Copy.svelte";
+
 
   import { onDestroy } from 'svelte';
 
+  import ProjectDashboard from '../Project/ProjectDashboard.svelte'
   import Sheets from '../Data/Sheets.svelte'
   import BlocklyEditor from '../App/BlocklyEditor.svelte'
   import RepositoryTab from "../Project/RepositoryTab.svelte"
@@ -20,22 +15,13 @@
   import NodeEditor from '../Story/NodeEditor.svelte'
   import Design from '../Design/Design.svelte'
   
-  import { InterkitClient } from 'interkit'
   import {
     currentProject,
-    secondaryTabIndex,
-    secondaryTabSpecialDoc,
-    currentProjectReadOnly,
-    projectManagerTab
   } from '../admin.js'
-  import { docsGo } from "../docs";
-  import { push } from "svelte-spa-router";
 
   export let projectId
   export let tab
   export let updatePreviewUserAuth
-
-  const userIsRole = InterkitClient.userIsRole
   
   let selected
   let editorFilesKey = "init"
@@ -53,102 +39,20 @@
 
   let scheduledeventsListNotification
 
-  const info = file => {
-    secondaryTabIndex.set(1)
-    secondaryTabSpecialDoc.set($currentProject.uiState.metafile[file].html)
-  }
+
 
   onDestroy(() => {
     console.log("destroying ProjectWorkspace")
     window.removeEventListener('message', messageListener)
   });
 
-  const filterDocLink = (url) => {
-    const docsURL = "https://docs.interkit.app"
-    if(url.includes(docsURL)) {
-      let docsPath = url.replace(docsURL, "")
-      console.log("docsGo with", docsPath)
-      docsGo(docsPath)
-    } else {
-      window.open(url, '_blank')
-    }
-  }
 
-  const duplicateProject = async () => {
-    console.log("duplicating project", projectId)
-    const newId = await InterkitClient.call("project.duplicate", {projectId})
-    if(newId) {
-      projectManagerTab.set(0)
-      push("/" + newId)
-    } else {
-      alert("There was an error, creating the new project.")
-    }
-  }
 
 </script>
 
 <!-- start -->
 <div class="scrollable" class:active={!tab}>
-  <div class="ProjectDashboard markdownContent">
-    <ButtonSet style="justify-content: end">
-      <!--Button
-        kind="tertiary"
-        size="small"
-        on:click={() => info('readme')}
-        icon={Information}
-        disabled={!$currentProject?.uiState?.metafile?.readme?.html}
-        >
-        Show Readme
-      </Button-->
-      {#if $currentProject?.uiState?.metafile?.project?.html}
-        <!--Button
-          kind="ghost"
-          size="field"
-          on:click={() => info('project')}
-          style="max-width: none; margin-left: 1px"
-          icon={OpenPanelFilledRight}
-          disabled={!$currentProject?.uiState?.metafile?.project?.html}
-          >
-          Show this text on the right
-        </Button-->
-      {/if}
-    </ButtonSet>
-    <div class="content">
-      {#if $currentProject?.uiState?.metafile?.project?.html}
-        <div on:click={(e)=>{filterDocLink(e.target.href); e.preventDefault()}}>
-        {@html $currentProject?.uiState?.metafile?.project?.html}
-        </div>
-      {:else}
-        <h1>
-        Welcome to {$currentProject?.name}
-        </h1>
-        This project/template does not provide an information file (project.md).
-      {/if}
-      {#if $currentProject?.isTemplate}
-        <div style="margin-top: 2em;">
-          <p>This project is a <Tag>template</Tag></p>
-          {#if $userIsRole?.admin}
-            <p>As an admin user, you can deactivate readonly mode to make changes directly to the template.
-            <Toggle 
-              labelText="Read only mode" 
-              toggled={$currentProjectReadOnly}
-              on:toggle={(e) => {
-                if(!e.detail.toggled) {
-                  currentProjectReadOnly.set(false)
-                } else {
-                  currentProjectReadOnly.set(true)
-                }
-              }}
-            /></p>
-          {/if}
-          <p>
-            You can create a new project based on this template
-            <Button size="small" icon={Copy} on:click={duplicateProject}>Create</Button>
-          </p>
-        </div>
-      {/if} 
-    </div>
-  </div>
+  <ProjectDashboard {projectId} />
 </div>
 
 <!-- sheets -->
