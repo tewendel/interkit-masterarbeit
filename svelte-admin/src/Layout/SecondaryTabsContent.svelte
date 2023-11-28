aa39c8f805286d2e241d148b47075a10bb082b0a
Sebastian Quack <info@interkit.app>
2023-11-28 15:04:15 +0100

    bugifx maximize button right pane, size cycle

--- svelte-admin/src/Layout/SecondaryTabsContent.svelte
+++ svelte-admin/src/Layout/SecondaryTabsContent.svelte
@@ -80,7 +80,7 @@
           iconDescription="go back"
           disabled={!$docsRouterCanNavigate.back}
           on:click={() => { docsGo(-1) }}
-          />
+        />
         <Button
           kind="ghost"
           icon={ArrowRight}
@@ -102,13 +102,15 @@
         </Button>
       {/if}
     <!-- \u00ad is a soft hyphens so the word breaks nicely to fit in minimized, narrow sidebar -->
-    <Button
-      kind="ghost"
-      iconDescription="cycle size"
-      on:click={() => { secondaryTabsSize.set(($secondaryTabsSize + 1) % secondaryTabsSizes.length) }}
-      icon={FitToWidth}
-      style="margin-left: auto"
-      />
+    {#if !$secondaryTabsMinimized}
+      <Button
+        kind="ghost"
+        iconDescription="cycle size"
+        on:click={() => { secondaryTabsSize.set(($secondaryTabsSize + 1) % secondaryTabsSizes.length) }}
+        icon={FitToWidth}
+        style="margin-left: auto"
+        />
+    {/if}
     <Button
       kind="ghost"
       iconDescription={$secondaryTabsMinimized ? "maxi\u00admize" : "minimize"}
