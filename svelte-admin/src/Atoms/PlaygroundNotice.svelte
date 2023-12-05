cc5ecd8ffbe5cb8f6a3505c242b0489ba9b310c9
Holger H <holgheiss@gmail.com>
2023-12-05 19:22:51 +0100

    fix playground notice

--- svelte-admin/src/Atoms/PlaygroundNotice.svelte
+++ svelte-admin/src/Atoms/PlaygroundNotice.svelte
@@ -3,20 +3,20 @@
   import { onMount } from 'svelte'
   import { Tag } from "carbon-components-svelte";
 
-  let dateString
+  let timestamp, dateString, date
   
   onMount( async () => {
-    const timestamp = await InterkitClient.call("system.getPlaygroundResetTimestamp")
+    timestamp = await InterkitClient.call("system.getPlaygroundResetTimestamp")
     console.log("timestamp:.", timestamp)
 
     // get local date string from timestamp
-    const date = new Date(timestamp * 1000)
+    date = new Date(timestamp * 1000)
     // format just the time hh:mm
     dateString = date.toLocaleDateString('de', { hour: 'numeric', minute: 'numeric' })
   })
 </script>
 
-{#if dateString}
+{#if timestamp && date && dateString}
   <Tag type="magenta">
     Playground resets<br>
     {dateString}
