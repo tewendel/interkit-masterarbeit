2efd229e7be86ba1fa8aa1bf95f48d96aeff184f
Jakob Wierzba <interkitgitlab@jakobwierzba.de>
2023-12-07 16:51:42 +0100

    more refactor

---
+++ sites/example/src/content/about/us/team.svelte.md
@@ -0,0 +1,5 @@
+# Team
+
+* Lorem
+* Ipsum
+* Dolores
---
+++ sites/example/svelte.config.js
@@ -0,0 +1,21 @@
+import { mdsvex } from 'mdsvex';
+import mdsvexConfig from './mdsvex.config.js';
+import adapter from '@sveltejs/adapter-node';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	extensions: ['.svelte', ...mdsvexConfig.extensions],
+
+	kit: {
+		adapter: adapter(),
+		alias: {
+			// this will match a directory and its contents
+			// (`my-directory/x` resolves to `path/to/my-directory/x`)
+			'$interkit': '../../packages/interkit',
+		}
+	},
+
+	preprocess: [mdsvex(mdsvexConfig)]
+};
+
+export default config;
