c7509ebdfcb2919cc25391cd616021a3a9211f53
Holger H <holgheiss@gmail.com>
2024-02-17 17:01:42 +0100

    add UserAutoLogin component

---
+++ packages/interkit/components/UserAutoLogin.svelte
@@ -0,0 +1,40 @@
+<script>
+  import { onMount } from 'svelte'
+  import { InterkitClient } from 'interkit'
+
+  // TODO shouldnt be necessary, derive from AppBase?
+  export let defaultLang = 'en'
+  export let defaultLangIndex = '0'
+  export let username
+  export let password
+  defaultLangIndex = +defaultLangIndex || 0
+
+  let userId = InterkitClient.userId;
+
+  const createUser = async () => {
+    const token = await InterkitClient.login({
+      username,
+      password,
+      projectData: {
+        lang: defaultLang,
+        langIndex: defaultLangIndex
+      }
+    })
+  }
+
+  $: {
+    console.log("UserLogin - user changed: ", $userId);
+    if(!$userId) {
+      createUser()  
+    }
+  }
+
+
+</script>
+
+{#if $userId}
+  {#key $userId}
+    <slot></slot>
+  {/key}
+{/if}
+
