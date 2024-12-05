3c366a3bc28d8aa8346bab1c88e030dfe9240f9c
Holger H <hi@intergestalt.info>
2024-12-05 16:26:38 +0100

    improve last build date detection

--- app-bundler/src/updaters/lastBuildDate.mjs
+++ app-bundler/src/updaters/lastBuildDate.mjs
@@ -3,6 +3,7 @@
  */
 
 import { promises as fs } from "fs";
+import path from "path";
 import interkit_server from "../interkit_server.mjs";
 import { getProjectPath } from "../filesystem.mjs";
 
@@ -10,22 +11,24 @@ const buildDirName = "public";
 
 const updateLastBuildDate = async function (projectId, watchedFiles) {
   // check if there is any changed file starting with buildDirName
-  const found = Object.keys(watchedFiles).some((file) => {
-    if (file.startsWith(buildDirName)) {
-      return true;
-    }
-  });
+  const found = Object.keys(watchedFiles).some((file) => 
+    file.startsWith(buildDirName)
+  );
 
   if (found) {
-    const dir = getProjectPath(projectId) + "/" + buildDirName;
-    const timestamp = await fs.stat(dir).then((stat) => stat.mtimeMs);
-    const date = new Date(timestamp);
-    console.log("updateLastBuild", projectId, date);
-    interkit_server.call("project.updateUiState", {
-      projectId,
-      section: "lastBuildDate",
-      data: date,
-    });
+    const dir = path.join(getProjectPath(projectId), buildDirName);
+    try {
+      const timestamp = await fs.stat(dir).then((stat) => stat.mtimeMs);
+      const date = new Date(timestamp);
+      console.log("updateLastBuild", projectId, date);
+      interkit_server.call("project.updateUiState", {
+        projectId,
+        section: "lastBuildDate",
+        data: date,
+      });
+    } catch (err) {
+      console.error(`Error updating build date for project ${projectId}:`, err);
+    }
   }
 };
 
